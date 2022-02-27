cb043d6e8b3a8ac112fc7d457e09b5a637219bc4
Amelia Kang <79126195+doHoaSen@users.noreply.github.com>
2022-02-27 21:35:22 +0900

    Update [2022.02.27] : Create 'Search' Option
    Need to update: debug and find the errors

--- Challenges_Part2/No.150_ArtGallery_2.py
+++ Challenges_Part2/No.150_ArtGallery_2.py
@@ -2,9 +2,9 @@
 #새로운 작가와 작품 추가할 수 있어야
 #작품이 판매되면 그 작품에 대한 데이터를 SQL 데이터베이스에서 삭제하고 별도의 텍스트 파일에 저장해야
 #사용자는 작가, 제작 재료 또는 가격으로 검색할 수 있어야 함
+
 from tkinter import *
 import sqlite3
-from turtle import width
 
 def add_artist():
     newname = artist_name.get()
@@ -37,8 +37,8 @@ def add_art():
     newarttitle = arttitle.get()
     newartmedium = artmedium.get()
     newartprice = artprice.get()
-    cursor.execute("INSERT INTO Art (aritistid, title, medium, price) \
-        VALUES (?, ?, ?, ?, ?)", (newartname, newarttitle, newartmedium, newartprice))
+    cursor.execute("INSERT INTO Art (artistid, title, medium, price) \
+        VALUES (?, ?, ?, ?)", (newartname, newarttitle, newartmedium, newartprice))
     db.commit()
     artname.delete(0, END)
     arttitle.delete(0, END)
@@ -61,12 +61,63 @@ def clear_window():
     outputwindow.delete(0,END)
 
 
+def viewartists():
+    cursor.execute("SELECT * FROM Artists")
+    for x in cursor.fetchall():
+        newrecord = str(x[0]) + ", " + str(x[1]) + ", " + str(x[2]) + ", " + str(x[3]) + ", " \
+            + str(x[4]) + ", " + str(x[5]) + "\n"
+        outputwindow.insert(END, newrecord)
+
+def viewart():
+    cursor.execute("SELECT * FROM Art")
+    for x in cursor.fetchall():
+        newrecord = str(x[0]) + ", " + str(x[1]) + ", " + str(x[2]) + ", " + str(x[3]) + ", " + str(x[4]) + "\n"
+        outputwindow.insert(END, newrecord)
+
+
+def search_artist():
+    select_artist = searchartist.get()
+    cursor.execute("SELECT Artists.name FROM Artists WHERE artistid = ?", [select_artist])
+    for x in cursor.fetchall():
+        newrecord = str(x[0]) + ", " + str(x[1]) + ", " + str(x[2]) + ", " + str(x[3]) + ", " + str(x[4]) + "\n"
+        outputwindow.insert(END, newrecord)
+    searchartist.delete(0,END)
+    searchartist.focus()
+
+
+def search_by_price():
+    minprice = selectmin.get()
+    maxprice = selectmax.get()
+    cursor.execute("""SELECT Art.pieceid, Artists.name, \
+        FROM Artists, Art WHERE Artist.artistid = Art.artistid AND Art.price >= ? AND \
+            Art.price <= ? """, [minprice, maxprice])
+    for x in cursor.fetchall():
+        newrecord = str(x[0]) + ", " + str(x[1]) + ", " + str(x[2]) + ", " + str(x[3]) + ", " + str(x[4]) + "\n"
+        outputwindow.insert(END, newrecord)
+    selectmin.delete(0, END)
+    selectmax.delete(0, END)
+    selectmin.focus()
+
+
+
+def sold():
+    f = open("SoldArt.txt", "a")
+    selectedpiece = soldpeice.get()
+    cursor.execute("SELECT * FROM Art WHERE pieceid = ?", [selectedpiece])
+    for x in cursor.fetchall():
+        newrecord = str(x[0]) + ", " + str(x[1]) + ", " + str(x[2]) + ", " + str(x[3]) + ", " + str(x[4]) + "\n"
+        f.write(newrecord)
+    f.close()
+    cursor.execute("DELETE FROM Art WHERE pieceid = ?", [selectedpiece])
+    db.commit()
+
+
 #main
 with sqlite3.connect("Art.db") as db:
     cursor = db.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS Artists(artistid integer PRIMARY KEY, name text, address text, town text, \
     county text, postcode text);")
-cursor.execute("CREATE TABLE IF NOT EXISTS Art (pieceid integer PRIMARY KEY, title text, medium text, price integer);")
+cursor.execute("CREATE TABLE IF NOT EXISTS Art (pieceid integer PRIMARY KEY, artistid integer, title text, medium text, price integer);")
 
    
 window = Tk()
@@ -136,7 +187,7 @@ artprice.place(x=850, y=120, width=80, height=25)
 addartbtn = Button(text="Add Piece: ", command = add_art)
 addartbtn.place(x=110, y=150, width=130, height=25)
 
-clearartbtn = Button(text="Add Piece: ", command = clear_art)
+clearartbtn = Button(text="Clear Pieces: ", command = clear_art)
 clearartbtn.place(x=250, y=150, width=130, height=25)
 
 clearoutputwindow = Button(text= "Clear Output", command= clear_window)
@@ -144,4 +195,46 @@ clearoutputwindow.place(x=1020, y=200, width=155, height=25)
 
 outputwindow = Listbox()
 outputwindow.place(x=10, y=200, width= 1000, height=350)
+
+viewallartists = Button(text="View All Artists", command= viewartists)
+viewallartists.place(x= 1020, y=230, width=155, height=25)
+
+viewallart = Button(text= "View All Art", command= viewart)
+viewallart.place(x=1020, y=260, width=155, height=25)
+
+
+
+#search
+searchartist = Entry(text="")
+searchartist.place(x=1020, y=300, width=50, height=25)
+searchartist_btn = Button(text="Search by Artist", command= search_artist)
+searchartist_btn.place(x=1075, y=300, width= 100, height=25)
+
+medium_ = StringVar(window)
+searchmedium = OptionMenu(window, medium_, "Oil", "Watercolour", "Ink", "Acrylic")
+searchmedium.place(x=1125, y=330, width= 50, height=25)
+
+
+min_lbl = Label(text="Min:")
+min_lbl.place(x=1020, y=360, width=75, height=25)
+
+max_lbl = Label(text="Max:")
+max_lbl.place(x=1100, y=360, width=75, height=25)
+
+selectmin = Entry(text="")
+selectmin.place(x=1020, y=380, width=75, height=25)
+
+selectmax = Entry(text="")
+selectmax.place(x=1100, y=380, width=75, height=25)
+
+searchprice_btn = Button(text="Search by Price", command= search_by_price)
+searchprice_btn.place(x=1020, y=410, width=155, height=25)
+
+soldpeice = Entry(text="")
+soldpeice.place(x=1020, y=450, width=50, height=25)
+
+soldbtn = Button(text="Sold", command= sold)
+soldbtn.place(x=1075, y=450, width=100, height=25)
+
 window.mainloop()
+db.close()
